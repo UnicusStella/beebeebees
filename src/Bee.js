a11e9a699dc4ecda99c6ea68b18a1459649ffede
UnicusStella <unicusstella@gmail.com>
2021-08-21 16:11:14 +0900

    add test

--- src/Bee.js
+++ src/Bee.js
@@ -2,8 +2,8 @@ const Grub = require('./Grub');
 
 class Bee extends Grub {
   // TODO.. 여기에 작성 하세요
-  constructor(food) {
-    super(food);
+  constructor() {
+    super();
     this.age = 5;
     this.color = 'yellow';
     this.job = 'Keep on growing';
